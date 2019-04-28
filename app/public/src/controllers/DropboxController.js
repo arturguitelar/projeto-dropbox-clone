98b7976581867b06c9ad7c2f44cfe30ea889ac95
Artur Killdragon <artur.guitelar@gmail.com>
2019-04-28 12:52:09 -0300

    Recebendo arquivos no node.js com Formidable.

--- app/public/src/controllers/DropboxController.js
+++ app/public/src/controllers/DropboxController.js
@@ -18,10 +18,51 @@ class DropboxController
 
         // input file
         this.inputFilesEl.addEventListener('change', event => {
-            console.log(event.target.files);
+            
+            this.uploadTasks(event.target.files);
 
             // mostra na tela
             this.snackModalEl.style.display = 'block';
         });
+    }
+
+    /**
+     * Recebe uma coleção de arquivos e retorna uma Promise com cada uma 
+     * das Promises dessa coleção.
+     * 
+     * @param {*} files
+     * @return {Promise} Promise. 
+     */
+    uploadTasks(files) {
+        let promises = [];
+
+        // Nota: [...files] converte a coleção recebida em files para um array
+        [...files].forEach(file => {
+            promises.push(new Promise((resolve, reject) => {
+                let ajax = new XMLHttpRequest();
+
+                ajax.open('POST', '/upload');
+
+                ajax.onload = event => {
+                    try {
+                        resolve(JSON.parse(ajax.responseText));
+                    } catch(e) {
+                        reject(e);
+                    }
+                };
+
+                ajax.onerror = event => {
+                    reject(event);
+                }
+
+                // Lendo o arquivo com FormData
+                let formData = new FormData();
+                formData.append('input-file', file);
+
+                ajax.send(formData);
+            }));
+        });
+
+        return Promise.all(promises);
     }
 }
